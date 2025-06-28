e8e79fa108fe5f678a5fc5f916b31aebae0ba22c
Trung Nghia <nh0cbum1234@gmail.com>
2025-06-28 14:10:10 +0700

    Thêm số lượng cho checkAll

--- checkall.js
+++ checkall.js
@@ -12,20 +12,29 @@ const spanAll = document.createElement("span");
 spanAll.textContent = "Chọn tất cả";
 checkAllElement.append(inputAll, spanAll);
 checkbox.insertBefore(checkAllElement, checkboxList);
+quantity = document.querySelector(".quantity");
 // Lấy toàn bộ checkbox-item
 const items = document.querySelectorAll(".item");
-
+let checkboxFocus = 0;
 checkAllElement.addEventListener("change", () => {
     items.forEach((item) => {
         item.checked = inputAll.checked;
     });
+    if (inputAll.checked) checkboxFocus = 10;
+    else checkboxFocus = 0;
+    quantity.textContent = checkboxFocus;
 });
+
 items.forEach((item) => {
     item.addEventListener("change", () => {
+        if (item.checked) {
+            checkboxFocus++;
+        } else checkboxFocus--;
         const allChecked = Array.from(items).every((item) => item.checked); // Nodelist không có every với some nên em chuyển sang mảng.
         const someChecked = Array.from(items).some((item) => item.checked);
         inputAll.checked = allChecked;
         inputAll.indeterminate = !allChecked && someChecked; // inderminat true thì nó mới gạch ngang
+        quantity.textContent = checkboxFocus;
     });
 });
 
